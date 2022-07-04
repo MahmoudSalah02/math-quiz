d8fe3d223ce6e04bad9cb7acb1e795cb43b2e3e1
mahmoud <mahmouds343@gmail.com>
2022-07-04 15:55:19 +0300

    wrote cleaner code using python3 conventions

--- app.py
+++ app.py
@@ -1,44 +1,71 @@
 import json
-import random 
 
-f = open('quiz.json')
-data = json.load(f)
 
-def print_line():
+def fetch_quiz_question(file_path):
+    """Fetches all quiz questions from a JSON file given the file path"""
+    try:
+        f = open(file_path)
+        quiz_questions = json.load(f)
+    except FileNotFoundError:
+        print("Wrong file or file path")
+    except ValueError:  # includes simplejson.decoder.JSONDecodeError
+        print('Decoding JSON has failed')
+    finally:
+        f.close()
+    return quiz_questions
+
+
+def print_dashed_line():
+    """Prints a dashed line"""
     print("---------------------")
 
-def display_questions(topic):
-    choices = ["A.", "B.", "C.", "D."]
-    topicData = data["quiz"][topic]
-    score = [0, len(topicData)]
-    for question in topicData:
-        questionData = topicData[question]
-        print(questionData["question"])
-        for choice, option in zip(choices, questionData["options"]):
-            print(choice + option)
-        answer = input("> Choose answer: ").upper()
-        indexNum = questionData["options"].index(questionData["answer"])
-        if (answer +"." == choices[indexNum]):
-            print("\nCorrect!")
-            score[0] += 1
-        else:
-            print("\nIncorrect...")
-            print("Answer is " + questionData["answer"] + "\n")
-        print_line()
-    return "you got " + str(score[0]) + " out of " + str(score[1])
+
+def validate_answer(user_answer, question_data):
+    """Accepts the user answer and data on a quetion and returns a boolean of whether the user's answer is correct"""
+    actual_answer = question_data["answer"]
+    index_of_actual_answer = question_data["options"].index(actual_answer)
+    character_of_actual_answer = chr(ord('a') + index_of_actual_answer)
+    if character_of_actual_answer == user_answer:
+        print("Correct")
+        return True
+    else:
+        print("False, answer is " + character_of_actual_answer.upper())
+        return False
+
+
+def display_topic_questions(topic):
+    """Accepts a topic from the user, prints the question along with the corresponding options, and returns the final score of the user"""
+    quiz_questions = fetch_quiz_question('quiz.json')
+    try:
+        topic_questions = quiz_questions["quiz"][topic]
+    except KeyError:
+        print("Topic not found")
+        return
+    score = 0
+    # loop through each question in the specified topic
+    for question_num in topic_questions:
+        question_data = topic_questions[question_num]
+        print(question_data["question"])
+
+        # loop through the multiple choice answers
+        character_counter = 0
+        for option in question_data["options"]:
+            current_character = chr(ord('a') + character_counter)
+            print(current_character.upper() + ". " + option)
+            character_counter += 1
+
+        user_answer = input("> Choose answer: ").lower()
+        if validate_answer(user_answer, question_data):
+            score += 1
+
+    percentage_score = score / len(topic_questions)
+    return percentage_score
 
 
 while True:
-    topic = input("> Choose topic (sport/maths): ").lower()
-    if (topic == "sport"):
-        print(display_questions(topic))
-    
-    elif (topic == "maths"):
-        print(display_questions(topic))
-    else:
-        print("topic not found...")
-    playAgain = input("Enter (y) to play again: ")
-    if (playAgain != "y"):
-        break;
-
-f.close()
+    topic = input("> Choose topic: ").lower()
+    print("Your final score is: " +
+          str(display_topic_questions(topic) * 100) + "%")
+    playAgain = input("Enter (y) to play again: ").lower()
+    if playAgain != "y":
+        break
