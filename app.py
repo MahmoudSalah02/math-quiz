b3447f006c4623987012aa3027c9c8171a1d91e5
mahmoud <mahmouds343@gmail.com>
2022-07-04 12:45:36 +0300

    asking the user all questions on chosen topic and adding a score feature

--- app.py
+++ app.py
@@ -7,32 +7,35 @@ data = json.load(f)
 def print_line():
     print("---------------------")
 
-def display_question(topic):
-    topicData = data["quiz"][topic]
-    questionNum = "q" + str(random.randint(1, len(topicData)))
-    print_line()
-    print(topicData[questionNum]["question"])
-    print_line()
+def display_questions(topic):
     choices = ["A.", "B.", "C.", "D."]
-    for choice, option in zip(choices, topicData[questionNum]["options"]):
-        print(choice + option)
-    answer = input("> Choose answer: ").upper()
-    indexNum = topicData[questionNum]["options"].index(topicData[questionNum]["answer"])
-    if (answer +"." == choices[indexNum]):
-        print("\nCorrect!")
+    topicData = data["quiz"][topic]
+    score = [0, len(topicData)]
+    for question in topicData:
+        questionData = topicData[question]
+        print(questionData["question"])
+        for choice, option in zip(choices, questionData["options"]):
+            print(choice + option)
+        answer = input("> Choose answer: ").upper()
+        indexNum = questionData["options"].index(questionData["answer"])
+        if (answer +"." == choices[indexNum]):
+            print("\nCorrect!")
+            score[0] += 1
+        else:
+            print("\nIncorrect...")
+            print("Answer is " + questionData["answer"] + "\n")
         print_line()
-    else:
-        print("\nIncorrect...")
-        print("Answer is " + topicData[questionNum]["answer"] + "\n")
-        print_line()
+    return "you got " + str(score[0]) + " out of " + str(score[1])
 
 
 while True:
     topic = input("> Choose topic (sport/maths): ").lower()
+    score = ""
     if (topic == "sport"):
-        display_question(topic)
+        print(display_questions(topic))
+    
     elif (topic == "maths"):
-        display_question(topic)
+        print(display_questions(topic))
     else:
         topic = input("topic not found...")
     playAgain = input("Enter (y) to play again: ")
